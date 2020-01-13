cd3454332227010064867b1a2567d6bb6c61e84a
mehrnaz <mehrnaz1995@gmail.com>
2020-01-13 21:42:23 +0800

    initial commit merged with random graph generator

---
+++ cmake-build-debug/CMakeFiles/Data_set_creator_for_mincostmaxflow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Data_set_creator_for_mincostmaxflow.dir/main.cpp.o"
+  "Data_set_creator_for_mincostmaxflow"
+  "Data_set_creator_for_mincostmaxflow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Data_set_creator_for_mincostmaxflow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
