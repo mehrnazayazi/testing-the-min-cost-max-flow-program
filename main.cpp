844419eec6c7b7edff398d173a12eccb95fb591d
mehrnaz <mehrnaz1995@gmail.com>
2020-01-13 22:02:47 +0800

    the main program is tested here

--- main.cpp
+++ main.cpp
@@ -41,6 +41,17 @@
 #include <iostream>
 using namespace std;
 
+
+int m2, n2;
+int Head[80200], Cost[80200], BackupCost[80200];
+int End[80200], Cap[80200];
+int Y[80200], Next[80200];
+int p[80200], l[80200];
+bool PathExists;
+int minCost = 0, flow2=0;
+
+
+
 // the maximum number of vertices + 1
 #define NN 1024
 
@@ -159,6 +170,169 @@ int mcmf4( int n, int s, int t, int &fcost )
     return flow;
 }
 
+
+
+
+void Dijkstra2(){
+    bool uncoloured[n2];
+    l[0] = 0;
+    p[0] = 0;
+    uncoloured [0] = true;
+    for (int i = 1; i < n2; ++i) {
+        p[i] = 0;
+        uncoloured[i] = true;
+        l[i] = 20000;
+    }
+    bool uncoloured_exist = true;
+    int u = 0;
+    int counter = 0;
+    while(uncoloured_exist){
+        counter++;
+        if(counter> 2 * m2){
+            if(l[n2 - 1] < 20000){
+                return;
+            }
+            PathExists = false;
+            return;
+        }
+        uncoloured[u] = false;
+        uncoloured_exist = false;
+        int next = Y[u];
+        while(next!=-1){
+            if(u == 17 && End[next] == 26){
+//                cout<<"cap of edge: "<<Cap[next]<<endl;
+            }
+            if(Cap[next]!=0){
+                int neighbor = End[next];
+                if(uncoloured[neighbor]){
+                    if(l[neighbor] > l[u]+ Cost[next]){
+                        l[neighbor] = l[u] + Cost[next];
+                        p[neighbor] = u;
+                    }
+                }
+            }
+            next = Next[next];
+        }
+        int min = 20000;
+        for (int i = 0; i < n2; ++i) {
+            if(uncoloured[i]){
+                uncoloured_exist = true;
+                if(l[i] < min){
+                    min = l[i];
+                    u = i;
+                }
+            }
+        }
+    }
+
+
+}
+
+
+void AddEdge(int head, int end, int cost, int cap, int name){
+    int i = 0, next;
+    Head[name] = head;
+    End[name] = end;
+    Cost[name] = cost;
+    Cap[name] = cap;
+    if( Y[head]== -1){
+        Y[head] = name;
+    }else{
+        int former = Y[head];
+        int next = Next[former];
+        while(next!= -1){//TODO change it to get back to head;
+            former = next;
+            next = Next[former];
+        }
+        Next[former] = name;
+    }
+}
+
+
+bool testProgram(int numV, int m, int flow,int cost, int Randcost[250][250],int Randcap[250][250]){
+    int  x, y, w, c;
+    n2 = numV;
+    m2 = m;
+    for (int i = 0; i < n2; ++i) {
+        p[i] = -1;
+    }
+    for(int i=0; i < n2; i++){
+        Y[i] = -1;
+    }
+    for(int i=0; i < 2 * m2; i++){
+        Next[i] = -1;
+    }
+    int numEdge = 0;
+    for (int i = 0; i < numV ; ++i) {
+        for (int j = 0; j < numV; ++j) {
+            if(Randcap[i][j]!=-1){
+                AddEdge(i,j,Randcost[i][j],Randcap[i][j],numEdge);
+                AddEdge(j,i,-Randcost[i][j],0,m2+numEdge);
+                numEdge++;
+            }
+        }
+    }
+    for (int k = 0; k < 2 * m2; ++k) {
+        BackupCost[k] = Cost[k];
+    }
+    PathExists = true;
+    int pathCost = 0, minCap = 20000;
+    while(PathExists){
+        int pathCost = 0;
+        Dijkstra2();
+        if(!PathExists){
+            break;
+        } else{
+            int i = n2 - 1;
+            while (i!=0){
+//                cout<<p[i]<<", ";
+                cout.flush();
+                i = p[i];
+            }
+//            cout<<endl;
+        }
+        int i = n2 - 1;
+        minCap = 200000;
+        while (i!=0){
+            for(int j=0;j< 2 * m2; j++){
+                if(End[j] == i && Head[j]==p[i]){
+                    if(Cap[j] < minCap){
+                        minCap = Cap[j];
+                        if(Cap[j] == 0){
+//                            cout<<"bottle neck= "<<Head[j]<<"-->>"<<End[j]<<endl;
+                        }
+                    }
+                    pathCost+=BackupCost[j];
+                    break;
+                }
+            }
+            i = p[i];
+        }
+//        cout<<"path min cap = "<< minCap<<endl;
+        i = n2 - 1;
+        while(i != 0){
+            for (int j = 0; j < 2 * m2 ; ++j) {
+                if(End[j] == i && Head[j]==p[i]){
+                    Cap[j]-=minCap;
+                }
+                if(End[j] == p[i] && Head[j]== i){
+                    Cap[j]+=minCap;
+                }
+            }
+            i = p[i];
+        }
+        minCost+=(pathCost*minCap);
+        flow2 += minCap;
+        for (int i = 0; i < 2 * m2; ++i) {
+            Cost[i] = Cost[i]+ l[Head[i]] - l[End[i]];
+        }
+    }
+    if(flow == flow2 && cost == minCost){
+        return true;
+    }
+    return false;
+}
+
 //----------------- EXAMPLE USAGE -----------------
 #include <iostream>
 #include <stdio.h>
@@ -228,9 +402,14 @@ int main()
 
 
     int fcost;
-    int flow = mcmf4( numV, s, t, fcost );
-    cout << "flow: " << flow << endl;
-    cout << "cost: " << fcost << endl;
+    int flow = mcmf4(numV, s, t, fcost );
+    if(testProgram(numV,m,flow,fcost,Randcost,Randcap)){
+        cout<<"True"<<endl;
+    }else{
+        cout<<"False"<<endl;
+    }
+//    cout << "flow: " << flow << endl;
+//    cout << "cost: " << fcost << endl;
 
     return 0;
 }
